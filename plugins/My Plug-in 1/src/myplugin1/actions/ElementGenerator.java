555d55aee02c0330707090ff40fc8ca536eb63c5
marius <mariusheinrichs@gmail.com>
2022-09-06 17:09:46 +0200

    Extended new Browser Action by creating another class with an enumeration and a dependency to first class

--- plugins/My Plug-in 1/src/myplugin1/actions/ElementGenerator.java
+++ plugins/My Plug-in 1/src/myplugin1/actions/ElementGenerator.java
@@ -7,6 +7,7 @@ import com.nomagic.magicdraw.openapi.uml.ReadOnlyElementException;
 import com.nomagic.magicdraw.openapi.uml.SessionManager;
 import com.nomagic.magicdraw.uml.Finder;
 import com.nomagic.uml2.ext.jmi.helpers.CoreHelper;
+import com.nomagic.uml2.ext.magicdraw.classes.mddependencies.Dependency;
 import com.nomagic.uml2.ext.magicdraw.classes.mdkernel.*;
 import com.nomagic.uml2.ext.magicdraw.classes.mdkernel.Class;
 import com.nomagic.uml2.ext.magicdraw.classes.mdkernel.Package;
@@ -28,8 +29,8 @@ public class ElementGenerator {
 
     public void execute(Package parentPackage) throws ReadOnlyElementException {
         try{
-            SessionManager.getInstance().createSession(project, "Creating a single Element");
-            createModelElement(parentPackage);
+            SessionManager.getInstance().createSession(project, "Creating a few Elements");
+            createModelElements(parentPackage);
             SessionManager.getInstance().closeSession(project);
         } catch (Exception e) {
             Application.getInstance().getGUILog().showMessage("Exception occured: " + e.getMessage());
@@ -37,11 +38,18 @@ public class ElementGenerator {
         }
     }
 
-    private void createModelElement(Package parentPackage) throws ReadOnlyElementException {
+    private void createModelElements(Package parentPackage) throws ReadOnlyElementException {
         Class firstClass = createClass(parentPackage, "First class");
-
         addProperty(firstClass);
         addOperation(firstClass);
+
+        Enumeration enumeration = createEnumeration(parentPackage);
+
+        Class secondClass = createClass(parentPackage, "Second class");
+
+        addEnumeration(secondClass, enumeration);
+
+        createRelation(parentPackage, firstClass, secondClass);
     }
 
     private Class createClass(Package parentPackage, String name) throws ReadOnlyElementException {
@@ -67,5 +75,42 @@ public class ElementGenerator {
         Operation operation = factory.createOperationInstance();
         operation.setName("myOperation");
         manager.addElement(operation, mdClass);
+    }
+
+    private Enumeration createEnumeration(Package parentPackage) throws ReadOnlyElementException {
+        Enumeration myEnum = factory.createEnumerationInstance();
+        myEnum.setName("My Enumeration");
+
+        String[] values = new String[]{"1", "2", "3"};
+
+        for(String value : values) {
+            EnumerationLiteral literal = factory.createEnumerationLiteralInstance();
+            LiteralString valueSpecification = factory.createLiteralStringInstance();
+            valueSpecification.setValue(value);
+
+            literal.setSpecification(valueSpecification);
+            literal.setName(value);
+            literal.setEnumeration(myEnum);
+        }
+
+        manager.addElement(myEnum, parentPackage);
+
+        return myEnum;
+    }
+
+    private void addEnumeration(Class mdClass, Enumeration enumeration) throws ReadOnlyElementException {
+        Property property = factory.createPropertyInstance();
+        property.setName("myEnumeration");
+        property.setType(enumeration);
+        CoreHelper.setMultiplicity(1, 1, property);
+
+        manager.addElement(property, mdClass);
+    }
+
+    private void createRelation(Package parentPackage, Class firstClass, Class secondClass) throws ReadOnlyElementException {
+        Dependency dependency = factory.createDependencyInstance();
+        CoreHelper.setSupplierElement(dependency, firstClass);
+        CoreHelper.setClientElement(dependency, secondClass);
+        dependency.setOwner(parentPackage);
     }
 }
